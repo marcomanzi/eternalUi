69b2a06d526f623a6e56e0f8c180ff432da03e8a
Marco Manzi <mmanzi@soldo.com>
2020-02-08 15:47:46 +0100

    Fix on build for deploy

--- prepare-project.sh
+++ prepare-project.sh
@@ -1,6 +1,6 @@
-mkdir -p node_modules/\@vaadin/flow-frontend/
 mvn vaadin:prepare-frontend -Pproduction
 mvn install -DskipTests -Pproduction
+mkdir -p node_modules/\@vaadin/flow-frontend/
 cp src/main/resources/META-INF/frontend/example-style.js node_modules/\@vaadin/flow-frontend/
 mvn install -DskipTests -Pproduction
 mvn package -DskipTests -Pproduction
