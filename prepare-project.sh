66e313f9e029ddc1e4dd163a8450a584af028ae1
Marco Manzi <mmanzi@soldo.com>
2020-02-12 16:38:31 +0100

    Fixes for vaadin build

--- prepare-project.sh
+++ prepare-project.sh
@@ -1,6 +1,4 @@
+rm -rf node-modules package* webpack*
 mvn vaadin:prepare-frontend -Pproduction
-mvn install -DskipTests -Pproduction
-mkdir -p node_modules/\@vaadin/flow-frontend/
-cp src/main/resources/META-INF/frontend/example-style.js node_modules/\@vaadin/flow-frontend/
 mvn install -DskipTests -Pproduction
 mvn package -DskipTests -Pproduction
