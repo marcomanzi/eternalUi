19e9711e1cd9ce5887117fad7cc856eb28029d29
Marco Manzi <mmanzi@soldo.com>
2020-02-13 18:51:48 +0100

    Deploy in staging fix and new version started

--- prepare-project.sh
+++ prepare-project.sh
@@ -3,3 +3,4 @@ rm -rf node_modules package* webpack*
 mvn vaadin:prepare-frontend -Pproduction
 mvn install -DskipTests -Pproduction
 mvn package -DskipTests -Pproduction
+mvn -DperformRelease=true deploy -Pproduction
